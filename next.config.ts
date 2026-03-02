1777732357b5640ec880031dc216e89a719a3370
disejo <devsplice@gmail.com>
2026-03-02 20:45:49 -0300

    020320262045

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  //eslint: {
+  //  ignoreDuringBuilds: true,
+  //},
   turbopack: {
     resolveAlias: {
       // Add any necessary aliases here if you're mapping '@mdi/js' to a different location
